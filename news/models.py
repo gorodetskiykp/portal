02e2cd93a4048c63a9eef38f150ffeb3344c89ce
GorodetskiyKP <gorodetskiykp@gmail.com>
2018-01-22 16:20:58 +1000

    Создал проект. Добавил приложение News (Новости).

---
+++ news/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Article(models.Model):
+    # Название
+    # Дата публикации
+    # Автор публикации - из модели Users
+    # Отдел публикации
+    # Фотографии
+    pass
+
+
